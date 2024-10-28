4d913eb93bd3959fdbf2ad2dfdd7181526e4b57b
PUNCHY <ucbvzp1@ucl.ac.uk>
2024-10-28 08:58:39 +0000

    add unit test

---
+++ test_times.py
@@ -0,0 +1,11 @@
+import datetime
+import times
+
+def test_given_input():
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    print(times.compute_overlap_time(large, short))
+
+    result = times.compute_overlap_time(large, short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
